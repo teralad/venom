c36582a6ace37ce8fe2b0cf624f4a6d41e62f782
Premkumar <prem.svmm@gmail.com>
2018-09-28 12:37:12 +0530

    handled exception

--- D.py
+++ D.py
@@ -3,8 +3,11 @@ import sys
 
 class D:
     def decode(self,LocationID):
-        decoded_value = base64.b64decode(LocationID).decode("utf-8")
-        return decoded_value.replace("Location:","")
+        try:
+            decoded_value = base64.b64decode(LocationID).decode("utf-8")
+            return decoded_value.replace("Location:","")
+        except:
+            return "Exception while decode"
 
 if __name__ == '__main__':
     d = D()
--- E.py
+++ E.py
@@ -3,9 +3,12 @@ import sys
 
 class E:
     def encode(self,Location):
-        encoded_value = base64.b64encode(bytes((f'Location:{Location}'), 'utf-8'))
-        return encoded_value.decode("utf-8")
+        try:
+            encoded_value = base64.b64encode(bytes((f'Location:{Location}'), 'utf-8'))
+            return encoded_value.decode("utf-8")
+        except:
+            return "exception in encode"
 
 if __name__ == '__main__':
-    e =E()
+    e = E()
     print(e.encode(sys.argv[1]))
