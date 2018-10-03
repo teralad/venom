0df11c169c033930640bf7b3f19d1c3c1e1dbc54
Nitesh Sharma <nitesh@synup.com>
2018-10-03 16:03:29 +0530

    Return proper db_id from decoded key

--- D.py
+++ D.py
@@ -5,9 +5,10 @@ class D:
     def decode(self,LocationID):
         try:
             decoded_value = base64.b64decode(LocationID).decode("utf-8")
-            return decoded_value.replace("Location:","")
-        except:
-            return "Exception while decode"
+            db_id = decoded_value.split(":")[-1]
+            return db_id
+        except Exception as e:
+            print("Error while decoding the key: {}".format(e))
 
 if __name__ == '__main__':
     d = D()
